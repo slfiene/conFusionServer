c9972bce356a8f105ba67ed35522d7b24a32946a
slfiene <sean.fiene@nintex.com>
2019-06-30 15:24:24 -0700

    Week3Assignment

--- models/favorites.js
+++ models/favorites.js
@@ -2,6 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const favoriteSchema = new Schema({
+    user:  {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    dishes:  [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Dish'
+    }]
+}, {
+    timestamps: true
 
 });
 
